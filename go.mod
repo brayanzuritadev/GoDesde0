8b9b19d78c5c18bd9c3b628d7b9a17ceb3eba9c9
Brayan Zurita <brayanzuritadev@gmail.com>
2023-10-16 23:10:25 -0400

    first commit

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/brayanzuritadev/GoDesde0
+
+go 1.21.3
---
+++ main.go
@@ -0,0 +1,11 @@
+package main
+
+import(
+	"fmt"
+	"github.com/brayanzuritadev/GoDesde0/variables"
+)
+
+func main() {
+	variables.MuestroEnteros()
+	fmt.Println("Hola mundo")
+}
